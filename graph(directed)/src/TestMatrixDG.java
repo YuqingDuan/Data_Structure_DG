7973514d51ce568d07105760f256476fcbbe6169
YuqingDuan <yduan@laurentian.ca>
2019-02-10 23:39:59 -0500

    The summay for data structure (DG part) !

---
+++ graph(directed)/src/TestMatrixDG.java
@@ -0,0 +1,17 @@
+public class TestMatrixDG {
+    public static void main(String[] args) {
+        char[] vertexes = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+        char[][] edges = {
+                {'A', 'C'},
+                {'A', 'D'},
+                {'A', 'F'},
+                {'B', 'C'},
+                {'C', 'D'},
+                {'E', 'G'},
+                {'F', 'G'}};
+
+        MatrixDG graph = new MatrixDG(vertexes, edges);
+
+        graph.print();
+    }
+}
